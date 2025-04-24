ba76d591e532196fbaf845b4cd81e50023465d6e
karimsak04 <slimanisak04@gmail.com>
2025-04-24 22:34:29 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,40 +1,67 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/gorilla/websocket"
+	"crypto/tls"
+	"log"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
-    CheckOrigin: func(r *http.Request) bool { return true },
+	HandshakeTimeout: 30 * time.Second,
+	CheckOrigin:      func(r *http.Request) bool { return true },
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Println("Upgrade error:", err)
-        return
-    }
-    defer conn.Close()
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+	defer conn.Close()
 
-    for {
-        mt, message, err := conn.ReadMessage()
-        if err != nil {
-            log.Println("Read error:", err)
-            break
-        }
-        log.Printf("Received: %s", message)
-        err = conn.WriteMessage(mt, message) // إعادة الإرسال (echo)
-        if err != nil {
-            log.Println("Write error:", err)
-            break
-        }
-    }
+	// Set timeouts
+	conn.SetReadDeadline(time.Now().Add(1 * time.Minute))
+	conn.SetWriteDeadline(time.Now().Add(1 * time.Minute))
+
+	for {
+		mt, message, err := conn.ReadMessage()
+		if err != nil {
+			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				log.Println("Read timeout:", err)
+			} else {
+				log.Println("Read error:", err)
+			}
+			break
+		}
+
+		log.Printf("Received: %s", message)
+		if err := conn.WriteMessage(mt, message); err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
 
 func main() {
-    http.HandleFunc("/ws", wsHandler)
-    log.Println("Listening on :8080...")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/ws", wsHandler)
+	http.HandleFunc("/healthz", healthCheck)
+
+	server := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  1 * time.Minute,
+	}
+
+	log.Println("Server starting on :8080...")
+	log.Fatal(server.ListenAndServe())
 }
